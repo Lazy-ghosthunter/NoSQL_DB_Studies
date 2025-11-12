5a87f23b93290774c7ea12af74a6d143782c8ecf
Ana <anakarolinesantana820@gmail.com>
2025-11-11 21:39:53 -0300

    outros exemplos

--- mongoDB/exemplosAtividades/agregar.mongodb.js
+++ mongoDB/exemplosAtividades/agregar.mongodb.js
@@ -1,7 +1,7 @@
 use('Fatec')
 
-//exemplo 1
-db.informatica.aggregate([
+//! exemplo 1
+/*db.informatica.aggregate([
     {
         $addFields: {
           dataISO: {$dateFromString:{dateString:"$Data", format: "%d/%m/%Y"}}
@@ -15,4 +15,65 @@ db.informatica.aggregate([
             }
         }
     }
-]).projection({_id:0});
+]).projection({_id:0});*/
+
+//! Exemplo 2
+/*db.informatica.aggregate([
+    {
+        $group:{
+            _id:"$Marca",
+            totalPreco:{$sum:"$Preço"}
+        }
+    },
+    {
+        $sort:{totalPreco:1}
+    }
+])*/
+
+//! Exemplo 3
+/*db.informatica.aggregate([
+    {
+        $group:{
+            _id: "$Material",
+            totalPreco:{$sum:"$Preço"}
+        }
+    },
+    {
+        $sort: {
+          _id: 1
+        }
+    }
+])*/
+
+//! Exemplo 4
+/*db.informatica.aggregate([
+    {
+        $addFields:{
+            valorTotal:{$multiply:["$Quantidade", "$Preço"]}
+        }
+    },
+    {
+        $sort:{Marca:1}
+    }
+]).projection({_id:0})*/
+
+//! Exemplo 5
+db.informatica.aggregate([
+    {
+        $addFields: {
+          DataISO:{
+            $dateFromString:{
+                dateString:"$Data", format:"%d/%m/%Y"
+            }
+          }
+        }
+    },
+    {
+        $match: {
+          DataISO:{
+            $gte: ISODate("2024-01-01T00:00:00Z"),
+            $lte: ISODate("2024-12-31T23:59:59Z")
+          }
+        }
+    }
+])
